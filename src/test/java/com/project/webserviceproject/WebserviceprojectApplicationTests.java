69b71f63e6a278c241df8a963ff2280c97d78c72
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-05-18 20:47:56 -0300

    Iniciando projeto webservice

---
+++ src/test/java/com/project/webserviceproject/WebserviceprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
