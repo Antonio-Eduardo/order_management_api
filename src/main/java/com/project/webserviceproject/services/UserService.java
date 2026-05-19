f407cfc6adcc99f863a2fa47aa40b2890e52b40b
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-05-18 22:54:59 -0300

    feat: findbyid

--- src/main/java/com/project/webserviceproject/services/UserService.java
+++ src/main/java/com/project/webserviceproject/services/UserService.java
@@ -3,9 +3,12 @@ package com.project.webserviceproject.services;
 import com.project.webserviceproject.entities.User;
 import com.project.webserviceproject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class UserService {
 
     @Autowired
@@ -13,5 +16,9 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+    public User FindById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
     }
 }
