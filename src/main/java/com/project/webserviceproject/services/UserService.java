41b94229fe1069b44fe4cf556cfbc08c6d46f915
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-05-20 19:53:10 -0300

    feat: trocando orElseThrow por try catch usando resourcenotfound e databaseexception

--- src/main/java/com/project/webserviceproject/services/UserService.java
+++ src/main/java/com/project/webserviceproject/services/UserService.java
@@ -2,8 +2,11 @@ package com.project.webserviceproject.services;
 
 import com.project.webserviceproject.entities.User;
 import com.project.webserviceproject.repositories.UserRepository;
+import com.project.webserviceproject.services.exceptions.DatabaseException;
 import com.project.webserviceproject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -27,9 +30,13 @@ public class UserService {
         return repository.save(obj);
     }
     public void deleteUser(Long id){
-        User user = repository.findById(id).orElseThrow(() ->new ResourceNotFoundException(id));
-
-             repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
 
     }
     public User updateUser(Long id, User obj){
