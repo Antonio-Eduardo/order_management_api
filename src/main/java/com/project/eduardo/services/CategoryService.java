92b82e25458edfdfadafbff366edff9d7965c620
Antonio Eduardo Moreira Oliveira <eduardo_m_cdc@hotmail.com>
2026-06-08 21:11:36 -0300

    feat: DTOresponse em CategoryService

--- src/main/java/com/project/eduardo/services/CategoryService.java
+++ src/main/java/com/project/eduardo/services/CategoryService.java
@@ -1,12 +1,16 @@
 package com.project.eduardo.services;
 
+import com.project.eduardo.dto.baseDTO.OrderItemDTO;
+import com.project.eduardo.dto.request.CategoryDTOrequest;
 import com.project.eduardo.dto.response.CategoryDTOresponse;
 import com.project.eduardo.dto.response.ProductDTOresponse;
 import com.project.eduardo.entities.Category;
 import com.project.eduardo.repositories.CategoryRepository;
 import com.project.eduardo.services.exceptions.ResourceNotFoundException;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
@@ -18,34 +22,26 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll() {
-        return repository.findAll();
-    }
+    public List<Category> findAll() {return repository.findAll();}
 
     public CategoryDTOresponse FindById(Long id) {
         Category obj = repository.findById(id).orElseThrow(() ->
                 new ResourceNotFoundException("Categoria não encontrada"
                         + id));
-        Set<ProductDTOresponse> products =
-                obj.getProducts().stream()
-                        .map(product -> {
-                            ProductDTOresponse dto = new ProductDTOresponse();
-                                    dto.setId(product.getId());
-                                    dto.setName(product.getName());
-                                    dto.setDescription(product.getDescription());
-                                    dto.setPrice(product.getPrice());
-                                    dto.setImgUrl(product.getImgUrl());
-                                    return dto;
-                        }).collect(Collectors.toSet());
         CategoryDTOresponse response = new CategoryDTOresponse();
         response.setId(obj.getId());
         response.setName(obj.getName());
-        response.setProducts(products);
-
         return response;
     }
 
-    public Category insertCategory(Category obj) {
-        return repository.save(obj);
+    @Transactional
+    public CategoryDTOresponse insertCategory(Category obj) {
+        CategoryDTOrequest request = new CategoryDTOrequest();
+        request.setName(obj.getName());
+        Category saved = repository.save(obj);
+        CategoryDTOresponse response = new CategoryDTOresponse();
+        response.setName(saved.getName());
+        response.setId(saved.getId());
+        return response;
     }
 }
