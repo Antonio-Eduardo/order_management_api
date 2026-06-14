d90486a9da73a3ceb54b936b71ad47d68bb9407f
Antonio Eduardo Moreira Oliveira <eduardo_m_cdc@hotmail.com>
2026-06-14 16:18:47 -0300

    fix: removendo payment class

--- src/main/java/com/project/eduardo/entities/Order.java
+++ src/main/java/com/project/eduardo/entities/Order.java
@@ -36,9 +36,6 @@ public class Order {
     @OneToMany(mappedBy = "id.order", cascade = CascadeType.ALL)
     private Set<OrderItem> items = new HashSet<>();
 
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
-
     public OrderStatus getOrderStatus() {
         return OrderStatus.valueOf(orderStatus);
     }
